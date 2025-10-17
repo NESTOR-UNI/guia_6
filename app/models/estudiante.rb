0962651d2d7188959c4cd165e6aaab574bacf811
NESTOR-UNI <pipotarea6@gmail.com>
2025-10-16 19:36:14 -0600

    first commit

---
+++ app/models/estudiante.rb
@@ -0,0 +1,15 @@
+class Estudiante < ApplicationRecord
+  # Validar que los campos no estén vacíos
+  validates :nombres, :apellidos, :carrera, :carnet, :fecha_nacimiento, :edad, :celular, presence: true
+
+  # Validar que la edad y el celular sean números enteros
+  validates :edad, numericality: { only_integer: true, less_than_or_equal_to: 999 }
+  validates :celular, numericality: { only_integer: true, less_than_or_equal_to: 88888888}
+
+  # (Opcional) Validar que la edad tenga máximo 3 dígitos
+  validates :edad, length: { maximum: 3 }
+  validates :celular, length: { maximum: 8}
+
+
+
+end
