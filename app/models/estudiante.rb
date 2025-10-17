5f0ab1eff341c3a06631a147cef65ebf3b7a23f0
NESTOR-UNI <pipotarea6@gmail.com>
2025-10-16 19:58:52 -0600

    Autofix: RuboCop style violations in estudiante.rb and routes.rb

--- app/models/estudiante.rb
+++ app/models/estudiante.rb
@@ -4,12 +4,8 @@ class Estudiante < ApplicationRecord
 
   # Validar que la edad y el celular sean números enteros
   validates :edad, numericality: { only_integer: true, less_than_or_equal_to: 999 }
-  validates :celular, numericality: { only_integer: true, less_than_or_equal_to: 88888888}
-
+  validates :celular, numericality: { only_integer: true, less_than_or_equal_to: 88888888 }
   # (Opcional) Validar que la edad tenga máximo 3 dígitos
   validates :edad, length: { maximum: 3 }
-  validates :celular, length: { maximum: 8}
-
-
-
-end
+  validates :celular, length: { maximum: 8 }
+  end
